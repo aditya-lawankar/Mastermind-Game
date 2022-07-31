66c605709eb0f2f6114e1f0eef2fb7b4be6edf58
Aditya Lawankar <70308865+aditya-lawankar@users.noreply.github.com>
2022-07-31 20:25:38 +0530

    Delete mkfile.mk

--- mkfile.mk
+++
@@ -1,8 +0,0 @@
-a.exe : main.o func.o colours.o
-	gcc main.o func.o colours.o
-main.o : main.c headers.h
-	gcc -c main.c
-func.o : func.c headers.h
-	gcc -c func.c
-colours.o : colours.c headers.h
-	gcc -c colours.c
